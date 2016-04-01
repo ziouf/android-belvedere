32161d0338cc962c64cb89eb34c4cf99c2415a08
Cyril <marin.cyril@gmail.com>
2016-04-01 17:09:18 +0200

    Maj de la géolocalisation pour corriger un bug provocant le recentrage de la camera maps sur la localisation de l'utilisateur de façon inopportune. Ajout de la permission CAMERA en prévision de l'implémentation de la partie AR

--- app/src/main/java/fr/marin/cyril/mapsapp/MapsActivity.java
+++ app/src/main/java/fr/marin/cyril/mapsapp/MapsActivity.java
@@ -4,20 +4,14 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
-import android.location.LocationListener;
 import android.location.LocationManager;
 import android.net.Uri;
-import android.support.design.widget.FloatingActionButton;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
-import android.text.util.Linkify;
-import android.view.MotionEvent;
 import android.view.View;
-import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -26,7 +20,6 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.io.InputStream;
 import java.util.ArrayList;
@@ -34,18 +27,17 @@ import java.util.Collection;
 import java.util.HashSet;
 
 import fr.marin.cyril.mapsapp.database.DatabaseHelper;
-import fr.marin.cyril.mapsapp.kml.parser.KmlParser;
 import fr.marin.cyril.mapsapp.kml.model.MapsMarker;
+import fr.marin.cyril.mapsapp.kml.parser.KmlParser;
 import fr.marin.cyril.mapsapp.tool.MapArea;
 
 public class MapsActivity extends FragmentActivity
         implements OnMapReadyCallback, GoogleMap.OnMapLoadedCallback, GoogleMap.OnCameraChangeListener {
 
-    private static final Long LOCATION_REFRESH_TIME = 10000L;
-    private static final Float LOCATION_REFRESH_DISTANCE = 25F;
+    private static final String LOCATION_PERMISSION = Manifest.permission.ACCESS_FINE_LOCATION;
 
-    private LocationManager locationManager;
     private GoogleMap mMap;
+    private LocationManager locationManager;
     private Collection<InputStream> kmlfiles;
     private Collection<Marker> markersShown;
 
@@ -60,15 +52,36 @@ public class MapsActivity extends FragmentActivity
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        this.dbHelper = new DatabaseHelper(this);
+        this.initSensorProviders();
+        this.initDataProviders();
+        this.initOnClickActions();
+
+    }
+
+    /**
+     *
+     */
+    private void initSensorProviders() {
+        this.locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+    }
+
+    /**
+     *
+     */
+    private void initDataProviders() {
+        this.markersShown = new HashSet<>();
 
         this.kmlfiles = new HashSet<>();
         this.kmlfiles.add(this.getResources().openRawResource(R.raw.sommets_des_alpes_francaises));
 
-        this.markersShown = new HashSet<>();
-
+        this.dbHelper = new DatabaseHelper(this);
         this.dbHelper.insertAllIfEmpty(new KmlParser().parseAll(this.kmlfiles));
+    }
 
+    /**
+     *
+     */
+    private void initOnClickActions() {
         // Action au click sur le bouton camera
         FloatingActionButton cameraButton = (FloatingActionButton) this.findViewById(R.id.camera_button);
         cameraButton.setOnClickListener(new View.OnClickListener() {
@@ -77,6 +90,8 @@ public class MapsActivity extends FragmentActivity
                 startActivity(new Intent(MapsActivity.this, CameraActivity.class));
             }
         });
+
+        // Add other actions below if needed
     }
 
     /**
@@ -92,14 +107,32 @@ public class MapsActivity extends FragmentActivity
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        mMap.setMyLocationEnabled(true);
+        if (this.checkPermission(LOCATION_PERMISSION)) {
+            mMap.setMyLocationEnabled(true);
+
+            Location l = this.locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            LatLng latLng = new LatLng(l.getLatitude(), l.getLongitude());
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 11));
+        }
+
         mMap.setOnMapLoadedCallback(this);
         mMap.setOnCameraChangeListener(this);
 
         mMap.getUiSettings().setMapToolbarEnabled(false);
         mMap.getUiSettings().setRotateGesturesEnabled(false);
 
-        mMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
+        mMap.setInfoWindowAdapter(this.getInfoWindowAdapter());
+        mMap.setOnInfoWindowClickListener(this.getOnInfoWindowClickListener());
+
+        this.updateMarkersOnMap();
+    }
+
+    /**
+     *
+     * @return
+     */
+    private GoogleMap.InfoWindowAdapter getInfoWindowAdapter() {
+        return new GoogleMap.InfoWindowAdapter() {
             @Override
             public View getInfoWindow(Marker marker) {
                 return null;
@@ -119,47 +152,21 @@ public class MapsActivity extends FragmentActivity
 
                 return v;
             }
-        });
+        };
+    }
 
-        mMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
+    /**
+     *
+     * @return
+     */
+    private GoogleMap.OnInfoWindowClickListener getOnInfoWindowClickListener() {
+        return new GoogleMap.OnInfoWindowClickListener() {
             @Override
             public void onInfoWindowClick(Marker marker) {
                 MapsMarker m = dbHelper.findByLatLng(marker.getPosition());
                 startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(m.getUrl())));
             }
-        });
-
-        this.updateMarkersOnMap(mMap);
-
-        // Obtention du gestionnaire de Geolocalisation
-        LocationListener mLocationListener = new LocationListener() {
-            @Override
-            public void onLocationChanged(Location location) {
-                LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
-                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 11));
-            }
-
-            @Override
-            public void onStatusChanged(String provider, int status, Bundle extras) {
-
-            }
-
-            @Override
-            public void onProviderEnabled(String provider) {
-
-            }
-
-            @Override
-            public void onProviderDisabled(String provider) {
-
-            }
         };
-        this.locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
-            this.locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
-                    LOCATION_REFRESH_TIME, LOCATION_REFRESH_DISTANCE, mLocationListener);
-
     }
 
     /**
@@ -167,7 +174,7 @@ public class MapsActivity extends FragmentActivity
      */
     @Override
     public void onMapLoaded() {
-        this.updateMarkersOnMap(mMap);
+        this.updateMarkersOnMap();
     }
 
     /**
@@ -176,14 +183,13 @@ public class MapsActivity extends FragmentActivity
      */
     @Override
     public void onCameraChange(CameraPosition cameraPosition) {
-        this.updateMarkersOnMap(mMap);
+        this.updateMarkersOnMap();
     }
 
     /**
      *
-     * @param mMap
      */
-    private void updateMarkersOnMap(GoogleMap mMap) {
+    private void updateMarkersOnMap() {
         MapArea area = new MapArea(mMap.getProjection().getVisibleRegion());
 
         this.removeOffScreenMarkers(area);
@@ -211,4 +217,11 @@ public class MapsActivity extends FragmentActivity
         markersShown.removeAll(toRemove);
     }
 
+    /**
+     *
+     * @return
+     */
+    private Boolean checkPermission(String perm) {
+        return ContextCompat.checkSelfPermission(MapsActivity.this, perm) == PackageManager.PERMISSION_GRANTED;
+    }
 }
