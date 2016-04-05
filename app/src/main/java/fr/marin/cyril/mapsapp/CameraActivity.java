ecbc636036c14edfd640c100f2c991cd9050387f
Cyril <marin.cyril@gmail.com>
2016-04-05 17:40:45 +0200

    [WIP] ancrage en bas de l'écran du textview de données de geolocalisation (mode debug)

--- app/src/main/java/fr/marin/cyril/mapsapp/CameraActivity.java
+++ app/src/main/java/fr/marin/cyril/mapsapp/CameraActivity.java
@@ -184,12 +184,6 @@ public class CameraActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
-        getWindow().getDecorView()
-                .setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE);
 
         // Bind database services
         this.bindService(new Intent(getApplicationContext(), DatabaseService.class),
