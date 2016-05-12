09a8766f8719455180f634cc8d147a2668c2950b
Cyril <marin.cyril@gmail.com>
2016-05-12 14:43:58 +0200

    Filtre passe bas pour stabiliser la boussole

--- app/src/main/java/fr/marin/cyril/belvedere/activities/CompassActivity.java
+++ app/src/main/java/fr/marin/cyril/belvedere/activities/CompassActivity.java
@@ -22,13 +22,13 @@ public class CompassActivity extends LocationActivity
     public static final int PITCH = 1;
     public static final String KEY_AZIMUTH = "azimuth";
     public static final String KEY_PITCH = "pitch";
+    private static final float ALPHA = 0.15f;
     protected final float[] data = new float[2];
     protected boolean portrait = true;
 
-    private HashSet<CompasEventListener> compasEventListenerSet;
+    private HashSet<CompasEventListener> compassEventListenerSet;
     private SensorManager sensorManager;
 
-    private long lastUpdate = 0;
     private float[] graMat;
     private float[] geoMat;
 
@@ -47,7 +47,7 @@ public class CompassActivity extends LocationActivity
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.compasEventListenerSet = new HashSet<>();
+        this.compassEventListenerSet = new HashSet<>();
         this.sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
     }
 
@@ -69,26 +69,24 @@ public class CompassActivity extends LocationActivity
     @Override
     public void onSensorChanged(SensorEvent event) {
         if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER)
-            graMat = event.values;
+            graMat = lowPass(event.values.clone(), graMat);
 
         if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD)
-            geoMat = event.values;
+            geoMat = lowPass(event.values.clone(), geoMat);
 
         if (graMat != null && geoMat != null) {
-            float[] oMat;
             float[] R = new float[9];
+            float[] oMat = new float[3];
             if (SensorManager.getRotationMatrix(R, null, graMat, geoMat)) {
-                if (!portrait)
+
+                if (!portrait) // Si mode paysage -> Réorientation de la matrice
                     SensorManager.remapCoordinateSystem(R, SensorManager.AXIS_X, SensorManager.AXIS_Z, R);
-                oMat = new float[3];
+
                 SensorManager.getOrientation(R, oMat);
                 this.data[AZIMUTH] = (float) Math.toDegrees(oMat[0]);
                 this.data[PITCH] = (float) Math.toDegrees(oMat[1]);
             }
         }
-
-        if ((lastUpdate + 250) > System.currentTimeMillis()) return;
-        lastUpdate = System.currentTimeMillis();
 
         if (location != null) {
             if (location.getExtras() == null) location.setExtras(new Bundle());
@@ -97,7 +95,7 @@ public class CompassActivity extends LocationActivity
         }
 
         // Run event
-        for (CompasEventListener eventListener : compasEventListenerSet)
+        for (CompasEventListener eventListener : compassEventListenerSet)
             eventListener.run();
     }
 
@@ -107,12 +105,21 @@ public class CompassActivity extends LocationActivity
     }
 
     protected CompasEventListener registerCompasEventListener(CompasEventListener eventListener) {
-        this.compasEventListenerSet.add(eventListener);
+        this.compassEventListenerSet.add(eventListener);
         return eventListener;
     }
 
     protected void unRegisterCompasEventListener(CompasEventListener eventListener) {
-        this.compasEventListenerSet.remove(eventListener);
+        this.compassEventListenerSet.remove(eventListener);
+    }
+
+    private float[] lowPass(float[] input, float[] output) {
+        if (output == null) return input;
+
+        for (int i = 0; i < input.length; ++i)
+            output[i] = output[i] + ALPHA * (input[i] - output[i]);
+
+        return output;
     }
 
     public abstract class CompasEventListener implements Runnable {
