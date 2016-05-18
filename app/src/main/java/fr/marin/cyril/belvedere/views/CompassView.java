c97ad694b119cc5ccf8e36d6ea216255c58434b7
Cyril <marin.cyril@gmail.com>
2016-05-18 17:31:57 +0200

    [WIP] CompassView

--- app/src/main/java/fr/marin/cyril/belvedere/views/CompassView.java
+++ app/src/main/java/fr/marin/cyril/belvedere/views/CompassView.java
@@ -1,34 +1,49 @@
 package fr.marin.cyril.belvedere.views;
 
 import android.content.Context;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
 
 /**
  * Created by CSCM6014 on 04/05/2016.
  */
-public class CompassView extends SurfaceView
-        implements SurfaceHolder.Callback {
+public class CompassView extends View {
     private final Context context;
-    private CompassPainter painter;
+
+    private Paint painter;
+
+    private float azimuth = 0f;
+
+    public CompassView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        this.context = context;
+
+        setUpPaint();
+        attrs.getAttributeFloatValue("", "", 0f);
+    }
 
     public CompassView(Context context) {
         super(context);
         this.context = context;
+
+        setUpPaint();
+    }
+
+    private void setUpPaint() {
+        painter = new Paint();
+        painter.setAntiAlias(true);
+        painter.setStrokeWidth(5);
+        painter.setStyle(Paint.Style.STROKE);
+        painter.setStrokeJoin(Paint.Join.ROUND);
+        painter.setStrokeCap(Paint.Cap.ROUND);
     }
 
     @Override
-    public void surfaceCreated(SurfaceHolder holder) {
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
 
     }
 
-    @Override
-    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-
-    }
-
-    @Override
-    public void surfaceDestroyed(SurfaceHolder holder) {
-
-    }
 }
