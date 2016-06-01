6b5290f471f4666fbe3f35e92d9319678979b49d
Cyril MARIN <marin.cyril@gmail.com>
2016-06-01 21:54:45 +0200

    [WIP] Refactorisation pour mise en place d'un NavigationDrawer
    Transformation de ActivityMaps en fragment
    Ajout de fragments pour les vues Settings et About

--- app/src/main/java/fr/marin/cyril/belvedere/activities/MainActivity.java
+++ app/src/main/java/fr/marin/cyril/belvedere/activities/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 import fr.marin.cyril.belvedere.R;
@@ -19,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
     public static android.support.v4.app.FragmentManager fragmentManager;
 
+    private Toolbar toolbar;
     private ActionBarDrawerToggle toggle;
     private DrawerLayout drawerLayout;
     private NavigationView navigationView;
@@ -27,7 +29,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        // Set a Toolbar to replace the ActionBar.
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+
 
         // Init Menu Drawer
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
