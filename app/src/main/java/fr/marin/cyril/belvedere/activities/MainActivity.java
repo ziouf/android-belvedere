39ded1290f72369e17df99daf84f17335a6b9d15
Cyril <marin.cyril@gmail.com>
2016-06-02 12:44:34 +0200

    [WIP] Refactorisation pour mise en place d'un NavigationDrawer
    Transformation de ActivityMaps en fragment
    Ajout de fragments pour les vues Settings et About

--- app/src/main/java/fr/marin/cyril/belvedere/activities/MainActivity.java
+++ app/src/main/java/fr/marin/cyril/belvedere/activities/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -25,18 +26,20 @@ public class MainActivity extends AppCompatActivity {
     private DrawerLayout drawerLayout;
     private NavigationView navigationView;
 
+    private Fragment currentFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeButtonEnabled(true);
-
         // Set a Toolbar to replace the ActionBar.
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
-
+        // Configuration de l'Actionbar
+        final ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayOptions(ActionBar.DISPLAY_HOME_AS_UP | ActionBar.DISPLAY_USE_LOGO);
+            actionBar.setHomeButtonEnabled(true);
+        }
 
         // Init Menu Drawer
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -67,34 +70,40 @@ public class MainActivity extends AppCompatActivity {
 
     private boolean selectDrawerItem(MenuItem item) {
         // Create a new fragment and specify the fragment to show based on nav item clicked
-        Fragment fragment = null;
-        Class fragmentClass;
         switch (item.getItemId()) {
             case R.id.menu_maps:
-                fragmentClass = MapsFragment.class;
+                popFragment(currentFragment);
                 break;
             case R.id.menu_settings:
-                fragmentClass = SettingsFragment.class;
+                currentFragment = addFragment(SettingsFragment.class);
                 break;
-            default:
-                fragmentClass = MapsFragment.class;
         }
-
-        try {
-            fragment = (Fragment) fragmentClass.newInstance();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        // Insert the fragment by replacing any existing fragment
-        fragmentManager.beginTransaction()
-                .replace(R.id.main_activity_fragment_placeholder, fragment)
-                .commit();
 
         // Close the navigation drawer
         drawerLayout.closeDrawers();
 
         return false;
+    }
+
+    private Fragment addFragment(Class fragmentClass) {
+        Fragment fragment = null;
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception ignore) {
+        }
+
+        fragmentManager.beginTransaction()
+                .add(R.id.main_activity_fragment_placeholder, fragment)
+                .commit();
+
+        return fragment;
+    }
+
+    private void popFragment(Fragment fragment) {
+        if (fragment != null)
+            fragmentManager.beginTransaction()
+                    .detach(fragment).remove(fragment)
+                    .commit();
     }
 
     @Override
