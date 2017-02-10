588d5df5acc3293d49a8295895c5c1732f7efe69
Cyril MARIN <cyril.marin@orange.com>
2017-02-10 13:49:52 +0100

    Ouverture de la page wikipedia en fonction de la langue de l'utilisateur

--- app/src/main/java/fr/marin/cyril/belvedere/dbpedia/QueryManager.java
+++ app/src/main/java/fr/marin/cyril/belvedere/dbpedia/QueryManager.java
@@ -2,6 +2,8 @@ package fr.marin.cyril.belvedere.dbpedia;
 
 import android.net.Uri;
 
+import java.util.Locale;
+
 /**
  * Created by cyril on 08/05/16.
  * <p>
@@ -10,38 +12,16 @@ import android.net.Uri;
  * - http://fr.dbpedia.org/sparql
  * - http://git.cm-cloud.fr/MARIN/belvedere/snippets/16
  * <p>
- * Selection de tous les sommets de France, groupés par Region
- * SELECT ?regionLabel ?nom ?altitude ?latitude ?longitude ?thumbnail ?wiki
- * WHERE {
- * ?peak rdf:type dbpedia-owl:Mountain .
- * ?peak rdf:type dbpedia-owl:NaturalPlace .
- * ?peak dbpedia-owl:region ?region .
- * ?region dbpedia-owl:country <http://fr.dbpedia.org/resource/France> .
- * ?peak dbpedia-owl:region ?region .
- * ?region rdfs:label ?regionLabel .
- * ?peak rdfs:label ?nom .
- * ?peak prop-fr:altitude ?altitude .
- * ?peak prop-fr:latitude ?latitude .
- * ?peak prop-fr:longitude ?longitude .
- * ?peak dbpedia-owl:thumbnail ?thumbnail .
- * ?peak foaf:isPrimaryTopicOf ?wiki .
- * FILTER(LANGMATCHES(LANG(?nom), "fr") && LANGMATCHES(LANG(?regionLabel), "fr"))
- * }
- * GROUP BY ?region
- * ORDER BY DESC(?region)
  */
 public class QueryManager {
+    private static final String lang = Locale.getDefault().getLanguage();
     private static final String SEP = " ";
-    private static final String API_URL = "http://dbpedia.org/sparql";
     private static final String RDF_PREFIX = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>";
     private static final String GEO_PREFIX = "PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>";
     private static final String DBO_PREFIX = "PREFIX dbo: <http://dbpedia.org/ontology/>";
     private static final String FOAF_PREFIX = "PREFIX foaf: <http://xmlns.com/foaf/0.1/>";
     public static final String PEAKS_QUERY =
             RDF_PREFIX + SEP + DBO_PREFIX + SEP + GEO_PREFIX + SEP + FOAF_PREFIX + SEP +
-                    "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>" + SEP +
-                    "PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>" + SEP +
-                    "PREFIX dbpedia-owl: <http://dbpedia.org/ontology/>" + SEP +
                     "SELECT ?id ?nom ?elevation ?latitude ?longitude ?wiki_url ?comment" + SEP +
                     "WHERE {" + SEP +
                     "?peak rdf:type dbo:Mountain ." + SEP +
@@ -52,7 +32,7 @@ public class QueryManager {
                     "?peak geo:long ?longitude ." + SEP +
                     "?peak foaf:isPrimaryTopicOf ?wiki_url ." + SEP +
                     "?peak rdfs:comment ?comment ." + SEP +
-                    "FILTER(LANGMATCHES(LANG(?nom), \"fr\") && LANGMATCHES(LANG(?comment), \"fr\"))" + SEP +
+                    "FILTER(LANGMATCHES(LANG(?nom), \"" + lang + "\") && LANGMATCHES(LANG(?comment), \"" + lang + "\"))" + SEP +
                     "}";
     public static final String MOUNTAINS_QUERY =
             RDF_PREFIX + SEP + DBO_PREFIX + SEP + GEO_PREFIX + SEP + FOAF_PREFIX + SEP +
@@ -67,14 +47,20 @@ public class QueryManager {
                     "?massif geo:lat ?latitude ." + SEP +
                     "?massif geo:long ?longitude ." + SEP +
                     "?massif foaf:isPrimaryTopicOf ?wiki_url ." + SEP +
-                    "FILTER(LANGMATCHES(LANG(?nom), \"fr\") && LANGMATCHES(LANG(?comment), \"fr\"))" + SEP +
+                    "FILTER(LANGMATCHES(LANG(?nom), \"" + lang + "\") && LANGMATCHES(LANG(?comment), \"" + lang + "\"))" + SEP +
                     "}";
 
     public static String buildApiUrl(String query) {
-        return API_URL + "?default-graph-uri=" +
-                "&query=" + Uri.encode(query) +
-                "&format=application%2Fsparql-results%2Bjson" +
-                "&timeout=0";
+        return new Uri.Builder()
+                .scheme("http")
+                .authority("dbpedia.org")
+                .appendPath("sparql")
+                .appendQueryParameter("default-graph-uri", "")
+                .appendQueryParameter("format", "application/sparql-results+json")
+                .appendQueryParameter("timeout", "0")
+                .appendQueryParameter("query", query)
+                .build()
+                .toString();
     }
 
 }
